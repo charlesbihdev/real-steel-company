766a901b5e4a9e897c992ea0d525a37d6e6da2b7
Charles Owusu Bih <charlesowusubih@gmail.com>
2024-02-04 18:26:10 +0000

    changed homepage style

--- scripts/togglemenu.js
+++ scripts/togglemenu.js
@@ -1,6 +1,9 @@
 const video = document.getElementById("myVideo");
 const toggleButton = document.getElementById("toggleButton");
 
+var scrollableDiv = document.getElementById("drop-sub-menu");
+scrollableDiv.scrollTop = scrollableDiv.scrollHeight;
+
 // Add a click event listener to the button
 toggleButton.addEventListener("click", function () {
   // Check if the video is paused
@@ -14,3 +17,59 @@ toggleButton.addEventListener("click", function () {
     toggleButton.src = "./icons/play.png";
   }
 });
+//blur-lg
+const bluredElements = document.getElementsByClassName("blur-element");
+// console.log(bluredElements);
+
+function handleMouseOverFunctions(menuType, arrowType) {
+  showdropdown(menuType);
+  // toggleArrow(arrowType);
+}
+
+function toggleArrow(idname) {
+  const arrow = document.getElementById(idname);
+  if (arrow.classList.contains("fa-angle-down")) {
+    arrow.classList.remove("fa-angle-down");
+    arrow.classList.add("fa-angle-right");
+  } else if (arrow.classList.contains("fa-angle-right")) {
+    arrow.classList.remove("fa-angle-right");
+    arrow.classList.add("fa-angle-down");
+  }
+}
+const arrow = document.getElementById("arrow1");
+const arrow2 = document.getElementById("arrow2");
+
+function showdropdown(menuType) {
+  document.getElementById("drop-menu").style.display = "block";
+
+  const dropdownMenu = document.getElementById(menuType);
+
+  arrow.classList.remove("fa-angle-down");
+  arrow.classList.add("fa-angle-right");
+
+  arrow2.classList.remove("fa-angle-right");
+  arrow2.classList.add("fa-angle-down");
+
+  if (menuType == "drop-sub-menu") {
+    arrow2.classList.remove("fa-angle-down");
+    arrow2.classList.add("fa-angle-right");
+  }
+
+  [...bluredElements].forEach((element) => {
+    element.classList.add("blur-lg");
+  });
+  console.log(menuType);
+  dropdownMenu.style.display = "block";
+}
+
+function hidedropdown(menuType) {
+  document.getElementById("drop-menu").style.display = "none";
+
+  const dropdownMenu = document.getElementById(menuType);
+  arrow.classList.remove("fa-angle-right");
+  arrow.classList.add("fa-angle-down");
+  dropdownMenu.style.display = "none";
+  [...bluredElements].forEach((element) => {
+    element.classList.remove("blur-lg");
+  });
+}
